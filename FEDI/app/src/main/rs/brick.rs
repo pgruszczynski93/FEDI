945d079a14c4f7b4e9151f52ea3f4fe43963667d
pgruszczynski93 <pgruszczynski93@gmail.com>
2017-07-21 00:41:17 +0200

    Filters added

--- FEDI/app/src/main/rs/brick.rs
+++ FEDI/app/src/main/rs/brick.rs
@@ -5,12 +5,20 @@
 #pragma rs_fp_relaxed
 
 float treshold;
-// progowanie po sredniej
+static const float Levels = 5.0f;
+static const float Mult = 0.003921569f;
+
+// zmienic listepoziomow
 uchar4 __attribute__((kernel)) brick(uchar4 pixel_in, uint32_t x, uint32_t y){
     float4 full_pix = rsUnpackColor8888(pixel_in);
     float3 rgb = full_pix.rgb;
-    float avg = (rgb.r + rgb.g + rgb.b)/3.0f;
-    avg = (avg >= treshold ? 1.0f : 0.0f);
-    rgb.r = rgb.g = rgb.b = avg;
+    float R = (((int) (rgb.r * 255.0f * 0.003921569f * Levels)) / Levels) * 255.0f;
+   	float G = (((int) (rgb.g * 255.0f * 0.003921569f * Levels)) / Levels) * 255.0f;
+   	float B = (((int) (rgb.b * 255.0f * 0.003921569f * Levels)) / Levels) * 255.0f;
+   	rgb.r = R/255.0f;
+   	rgb.g = G/255.0f;
+   	rgb.b = B/255.0f;
+
     return rsPackColorTo8888(rgb);
 }
+
