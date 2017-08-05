5fadd45b1b3dd72c7e68bf0b28ec560610507f11
pgruszczynski93 <pgruszczynski93@gmail.com>
2017-08-05 11:45:24 +0200

    Emboss repaired

--- FEDI/app/src/main/rs/emboss_relief_filter.rs
+++ FEDI/app/src/main/rs/emboss_relief_filter.rs
@@ -6,6 +6,7 @@
 
 rs_allocation img_in;
 int32_t width, height, mask_type;
+static float mask_0[9] = {-1,0,1, -1,0,1, -1,0,1};
 static float mask_1[9] = { -2, -2, -0, -2, 6, 0, 0, 0, 0};
 static float mask_2[9] = { -4, -4, -0, -4, 12, 0, 0, 0, 0};
 static float mask_3[9] = { 0, 0, 0, 0, 6, -2, 0, -2, -2};
@@ -32,53 +33,58 @@ uchar4 __attribute__((kernel)) emboss_relief_filter(uchar4 in, uint32_t x, uint3
     float* mask;
 
     if(!mask_type){
-        mask = mask_1;
+        mask = mask_0;
     }
     else if(mask_type==1){
-        mask = mask_2;
+        mask = mask_1;
     }
     else if(mask_type==2){
-        mask = mask_3;
+        mask = mask_2;
     }
     else if(mask_type==3){
-        mask = mask_4;
+        mask = mask_3;
     }
     else if(mask_type==4){
-        mask = mask_5;
+        mask = mask_4;
     }
     else if(mask_type==5){
-        mask = mask_6;
+        mask = mask_5;
     }
     else if(mask_type==6){
-        mask = mask_7;
+        mask = mask_6;
     }
     else if(mask_type==7){
-        mask = mask_8;
+        mask = mask_7;
     }
     else if(mask_type==8){
-        mask = mask_9;
+        mask = mask_8;
     }
     else if(mask_type==9){
-        mask = mask_10;
+        mask = mask_9;
     }
     else if(mask_type==10){
-        mask = mask_11;
+        mask = mask_10;
     }
     else if(mask_type==11){
+        mask = mask_11;
+    }
+    else if(mask_type==12){
         mask = mask_12;
     }
-
     if(pos_x >= 1 && pos_x < width-1 && pos_y >= 1 && pos_y < height-1){
         int32_t kernel_index = 0;
         for(int32_t k=-1; k<=1; k++){
             for(int32_t l=-1; l<=1; l++){
                 rgba = rsUnpackColor8888(*(const uchar4*)rsGetElementAt(img_in, pos_x + l, pos_y + k));
-                if(mask_type < 4){
+                if(mask_type <= 4){
                     rgba.rgb = GrayScaleValue01(rgba.rgb);
                 }
                 new_rgb += rgba.rgb * (*(mask+kernel_index));
                 ++kernel_index;
             }
+        }
+        if(mask_type<=4){
+            new_rgb += 0.5f;
         }
         new_rgb = Clamp01Float3(new_rgb);
     }
