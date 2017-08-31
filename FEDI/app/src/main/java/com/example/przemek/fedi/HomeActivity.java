5eaf548de7a789a606e591bd4d01753b42088049
pgruszczynski93 <pgruszczynski93@gmail.com>
2017-09-01 00:49:50 +0200

    Intro fixes

--- FEDI/app/src/main/java/com/example/przemek/fedi/HomeActivity.java
+++ FEDI/app/src/main/java/com/example/przemek/fedi/HomeActivity.java
@@ -2,8 +2,11 @@ package com.example.przemek.fedi;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,12 +15,31 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HideNotifAndTitleBars();
         setContentView(R.layout.activity_home);
 
         LoadMainMenu();
 
     }
 
+    void HideNotifAndTitleBars(){
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("FEDI");
+        if (actionBar != null) {
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(false);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
     /***
      * This method is used to finish current intent and start main menu activity after 1.5s.
      */
